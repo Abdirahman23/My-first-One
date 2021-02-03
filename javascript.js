a43190b96780bb4ef6f0a71bd36ad99a8b5f2cfe
Abdirahman Saed Abdilahi <77121312+Abdirahman23@users.noreply.github.com>
2021-02-03 07:13:35 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector(".menu")
+let nav = document.querySelector(".nav")
+
+console.log(nav)
+
+function myFunction(){
+    menu.classList.toggle("change")
+    nav.classList.toggle("change")
+    
+}
+
+
+
